6f69ac876f8067680024fb62ea4addb9a4a6fccc
Svetik-mne <svetlanagrizdak@yandex.ru>
2025-05-30 10:28:33 +0200

    Update precode_1.py

--- precode_1.py
+++ precode_1.py
@@ -6,6 +6,6 @@ from miles import HobbitsPriorityStatus
 class TestHobbitsPriorityStatus:
 
     # добавь декоратор
-    ...
-    def test_priority_status(self, ...): # передай параметры из декоратора в функцию
+@pytest.mark.parametrize('hobbit, priority_status', ['Фродо', 'Platinum'], ['Сэм', 'Gold'], ['Мэри', 'Silver'], ['Пиппин', 'Silver'])    
+    def test_priority_status(self, hobbit, priority_status): # передай параметры из декоратора в функцию
         assert HobbitsPriorityStatus().get_status(hobbit) == priority_status
